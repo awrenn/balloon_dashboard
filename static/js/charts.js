fbd4d4fec8f37822b96fb5d35a2fa373d4de1edc
Andrew Wrenn <awrenn53@gmail.com>
2019-08-13 07:40:11 -0700

    Still WIP Obviously

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,8 +1,28 @@
-var data = {
-  labels : [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri' ],
-  series : [ [ 5, 2, 4, 2, 0 ] ]
-};
+var times = [ 1 ];
 
-var options = {width : 300, height : 200};
+function core_loop() {
+  update_time_graph();
+  setTimeout(function() {
+    console.log("looping");
+    core_loop()
+  }, 1000);
+}
 
-new Chartist.Line('.ct-chart', data, options);
+function update_time_graph() {
+  m = times[times.length - 1] + 1;
+  times.push(m);
+  times = chop(times, 5);
+  var data = {labels : times, series : [ times ]};
+  var options = {width : 300, height : 200};
+  new Chartist.Line('#time_chart', data, options);
+}
+
+function chop(arr, c) {
+  if (arr.length <= c) {
+    return arr;
+  };
+  arr = arr.slice(arr.length - c, arr.length);
+  return arr;
+}
+
+core_loop()
